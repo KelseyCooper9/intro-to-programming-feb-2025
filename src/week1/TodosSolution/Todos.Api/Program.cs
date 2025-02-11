6bf54578322a90b112e36e5fa0717b3d2daa89a2
Kelsey Cooper <KelseyJCooper89@gmail.com>
2025-02-11 15:21:40 +0000

    update w/ jeffs code

--- src/week1/TodosSolution/Todos.Api/Program.cs
+++ src/week1/TodosSolution/Todos.Api/Program.cs
@@ -1,15 +1,14 @@
 
 using Marten;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Todos.Api.Todos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthorization();
-var connectionString = builder.Configuration.GetConnectionString("todos") ??
-    throw new Exception("Can't start the api without a connection string");
 
+var connectionString = builder.Configuration.GetConnectionString("todos") ?? 
+    throw new Exception("CAn't start the api without a connection string");
 builder.Services.AddMarten(builder =>
 {
     builder.Connection(connectionString);
@@ -18,7 +17,9 @@ builder.Services.AddMarten(builder =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// above this line is configuration for the services inside our application
 var app = builder.Build();
+// after this line is configuration for how HTTP requests and responses and handled.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,4 +29,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 app.MapTodos();
-app.Run();  //"Blocked here.
+
+app.Run(); // "Blocked" here. 
+
