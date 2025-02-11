d5e72f966f4cd38ada323f895f313c3e72da745b
Kelsey Cooper <KelseyJCooper89@gmail.com>
2025-02-11 19:47:04 +0000

    another test that works

--- src/week1/TodosSolution/Todos.Tests/Todos/GettingTodos.cs
+++ src/week1/TodosSolution/Todos.Tests/Todos/GettingTodos.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using Alba;
+using Todos.Api.Todos;
 
 namespace Todos.Tests.Todos;
 public class GettingTodos
@@ -10,10 +11,41 @@ public class GettingTodos
     public async Task GetsAOkStatusCode()
     {
         var host =  await AlbaHost.For<Program>();
+        //a bit of time between these 2
         await host.Scenario(api =>
         {
             api.Get.Url("/todos");
             api.StatusCodeShouldBeOk(); //200
         });
+
+     
+    }
+
+    [Fact]
+    public async Task CanAddAnItemToTodoList()
+    {
+        var host = await AlbaHost.For<Program>();
+        var itemToAdd = new TodoListCreateItem
+        {
+            Description = "Make Tacos " + Guid.NewGuid(),
+        };
+        await host.Scenario(api =>
+        {
+            api.Post.Json(itemToAdd).ToUrl("/todos");
+            api.StatusCodeShouldBeOk();
+        });
+
+        var getResponse = await host.Scenario(api =>
+        {
+            api.Get.Url("/todos");
+        });
+
+        var listofTodos = getResponse.ReadAsJson<List<TodoListItem>>(); // ? mark when hovering ovr listoftodos means it might get nothing back- might come back null
+
+        Assert.NotNull(listofTodos);
+
+        var hasMyItem =listofTodos.Any(item => item.Description == itemToAdd.Description);
+
+        Assert.True(hasMyItem);
     }
 }
