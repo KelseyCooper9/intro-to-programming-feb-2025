4e6814a2c70be811fc446642173c2224272a481f
Kelsey Cooper <KelseyJCooper89@gmail.com>
2025-02-12 21:34:40 +0000

    Kata- create and fail then find path to pass for first 4 'tests'

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿
+public class Calculator
+{
+    public int Add(string number)
+    {
+        if (number == "")
+        {
+            return 0;
+        }
+        else
+        {
+
+            string[] separate = number.Split(',');
+            var listOfInts = number.Split(',').Select(Int32.Parse).ToList();
+            int sum = 0;
+            foreach (int listofInt in listOfInts)
+            {
+                sum += listofInt;
+            }
+            return sum;
+        }
+        //int firstStringToNumber =Convert.ToInt32(separate[0]);
+        //int secondStringToNumber =Convert.ToInt32(separate[1]);
+        //return firstStringToNumber + secondStringToNumber;
+        //int stringToNumber = Convert.ToInt32(number);
+
+    }
+}
---
+++ src/week1/string-calculator-weds/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+    [Theory]
+    [InlineData("5")]
+    [InlineData("10")]
+    [InlineData("-5")]
+    public void SingleStringReturnsNumber(string number)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("5");
+        Assert.Equal(5, result);
+    }
+    [Fact]
+    public void AddTwoStringsTogether()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("5, 6");
+        Assert.Equal(11, result);
+    }
+    [Fact]
+    public void CanTakeArbitraryLength()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("5, 6, 7, 8");
+        Assert.Equal(26, result);
+    }
+}
