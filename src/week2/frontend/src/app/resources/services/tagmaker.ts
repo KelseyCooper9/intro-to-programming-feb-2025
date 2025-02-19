66c55c8157f17e49de5de6142ebfe3aa892fddc1
Kelsey Cooper <KelseyJCooper89@gmail.com>
2025-02-19 14:51:15 +0000

    jeffs test answer

--- src/week2/frontend/src/app/resources/services/tagmaker.ts
+++ src/week2/frontend/src/app/resources/services/tagmaker.ts
@@ -1,5 +1,19 @@
 export function tagMaker(tagList: string): string[] {
-  return ['dog', 'cat', 'mouse'];
+  // split the string into an array with, get rid of empty strings, no duplicates, converted to lower case
+
+  //  return tagList.split(' ').filter((tag, index, self) => tag && self.indexOf(tag) === index).map(tag => tag.toLowerCase());
+
+  const parts = tagList
+
+    .split(' ')
+
+    .filter((t) => t !== '')
+
+    .map((t) => t.trim())
+
+    .map((t) => t.toLowerCase());
+
+  return Array.from(new Set(parts));
 }
 
 // angular frontend-end programming
